07bb405212bc74960c69950bc30c26b85a63823e
TranThanhKhang <khangkt666@gmail.com>
2024-03-14 05:17:20 +0700

    fix remaining error due to merge

--- app/src/main/java/com/example/tk_app/customer/OrderAdapter.kt
+++ app/src/main/java/com/example/tk_app/customer/OrderAdapter.kt
@@ -7,7 +7,6 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.example.tk_app.OrderDetailActivity
 import com.example.tk_app.OrderModel
 import com.example.tk_app.R
 import com.example.tk_app.classify_product.accessory.DetailAccessoryActivity
